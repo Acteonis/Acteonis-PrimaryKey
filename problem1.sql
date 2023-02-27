73b83ab124b2173baf2e4c7cb09686bc02506e2f
Acteonis <adosminkina@gmail.com>
2023-02-27 04:03:51 +0000

    The lab is completed

---
+++ problem1.sql
@@ -0,0 +1,11 @@
+create table Song(
+    id SERIAL PRIMARY KEY,
+    title varchar(100),
+    artist varchar(100));
+  
+    /*insert into Song(title, artist) values('Let it Be','Beatles');
+    insert into Song(title, artist) values('Hotel California','Eagles');
+    insert into Song(title, artist) values('Kashmir','Led Zeppelin');*/
+
+
+
